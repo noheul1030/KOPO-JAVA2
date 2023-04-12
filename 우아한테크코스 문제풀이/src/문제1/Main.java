81c91815a7733f0ad8d8e9a7c83a8568769e9b8d
noheul1030 <papooa5140@gmail.com>
2023-04-12 18:07:46 +0900

    REFACTOR: 2. solution()

--- 우아한테크코스 문제풀이/src/문제1/Main.java
+++ 우아한테크코스 문제풀이/src/문제1/Main.java
@@ -2,7 +2,6 @@ package 문제1;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class Main {
 
@@ -10,18 +9,30 @@ public class Main {
 
 		Problem1 pp = new Problem1();
 
-//		pp.solution(pp.pobi, pp.crong);
+		pp.solution(pp.pobi(), pp.crong());
 	}
 
 }
+
 // 1. class Problem1
 class Problem1 {
-	
+
 	// 2. 메서드들을 모아서 최종 결과값을 출력하는 메서드
 	int solution(List<Integer> pobi, List<Integer> crong) {
+		System.out.printf("%6s%6s%6s%6s%6s\n", "pobi", " ", "crong", " ", "result");
+		int answer = Integer.MAX_VALUE;
 		
-		int answer = finalGame(pobi_Max(pobi()), crong_Max(crong()));
-
+		for (int i = 0; i < 1; i++) {
+			if (pobi.get(i)+1 != pobi.get(i + 1) || crong.get(i) + 1 != crong.get(i + 1)) {
+				answer = -1;
+				System.out.println(pobi + "  " + crong + "   " + answer);
+				break;
+			} 
+			else {
+				answer = finalGame(pobi_Max(pobi), crong_Max(crong));
+				System.out.println(pobi + "  " + crong + "   " + answer);
+			}
+		}
 		return answer;
 	}
 
@@ -36,7 +47,7 @@ class Problem1 {
 	// 4. pobi의 리스트에 random page를 저장하는 메서드
 	public List<Integer> pobi() {
 		List<Integer> pobi = new ArrayList<Integer>();
-		
+
 		while (true) {
 			int random = this.random();
 			if (random == 1 || random == 2 || random == 399 || random == 400) {
@@ -49,7 +60,6 @@ class Problem1 {
 					pobi.add(random);
 					pobi.add(random + 1);
 				}
-//				break;
 			}
 			return pobi;
 		}
@@ -59,7 +69,7 @@ class Problem1 {
 	// 5. crong의 리스트에 random page를 저장하는 메서드
 	public List<Integer> crong() {
 		List<Integer> crong = new ArrayList<Integer>();
-		
+
 		while (true) {
 			int random = this.random();
 			if (random == 1 || random == 2 || random == 399 || random == 400) {
@@ -72,7 +82,6 @@ class Problem1 {
 					crong.add(random);
 					crong.add(random + 1);
 				}
-//				break;
 			}
 			return crong;
 		}
@@ -81,83 +90,85 @@ class Problem1 {
 /////////////////////////////////////////////////////////////////////////////////
 	// 6. pobi의 리스트에 담긴 값에서 큰 수를 찾아내는 메서드
 	public int pobi_Max(List<Integer> pobi) {
-		
+
 		int left = 0, right = 0, pobi_Max = 0;
 
 		for (int i = 0; i < pobi.size(); i++) {
-			if (i == 0) 
+			if (i == 0)
 				left = pobi.get(i);
-			else 
+			else
 				right = pobi.get(i);
 		}
-		
+
 		String left2 = Integer.toString(left);
 		String right2 = Integer.toString(right);
-		
-		int[] sumul = {0,1,0,1};
-		
+
+		int[] sumul = { 0, 1, 0, 1 };
+
 		for (int i = 0; i < left2.length(); i++) {
-			sumul[0] += (int)left2.charAt(i)-'0';
-			sumul[1] *= (int)left2.charAt(i)-'0';
+			sumul[0] += (int) left2.charAt(i) - '0';
+			sumul[1] *= (int) left2.charAt(i) - '0';
 		}
 		for (int i = 0; i < right2.length(); i++) {
-			sumul[2] += (int)right2.charAt(i)-'0';
-			sumul[3] *= (int)right2.charAt(i)-'0';
+			sumul[2] += (int) right2.charAt(i) - '0';
+			sumul[3] *= (int) right2.charAt(i) - '0';
 		}
-		
+
 		for (int i = 0; i < sumul.length; i++) {
-			if(pobi_Max < sumul[i]) {
+			if (pobi_Max < sumul[i]) {
 				pobi_Max = sumul[i];
 			}
 		}
 
 		return pobi_Max;
 	}
+
 /////////////////////////////////////////////////////////////////////////////////
 	// 7. crong의 리스트에 담긴 값에서 큰 수를 찾아내는 메서드
 	public int crong_Max(List<Integer> crong) {
-		
+
 		int left = 0, right = 0, crong_Max = 0;
-		
+
 		for (int i = 0; i < crong.size(); i++) {
-			if (i == 0) 
+			if (i == 0)
 				left = crong.get(i);
-			else 
+			else
 				right = crong.get(i);
 		}
-		
+
 		String left2 = Integer.toString(left);
 		String right2 = Integer.toString(right);
-		
-		int[] sumul = {0,1,0,1};
-		
+
+		int[] sumul = { 0, 1, 0, 1 };
+
 		for (int i = 0; i < left2.length(); i++) {
-			sumul[0] += (int)left2.charAt(i)-'0';
-			sumul[1] *= (int)left2.charAt(i)-'0';
+			sumul[0] += (int) left2.charAt(i) - '0';
+			sumul[1] *= (int) left2.charAt(i) - '0';
 		}
 		for (int i = 0; i < right2.length(); i++) {
-			sumul[2] += (int)right2.charAt(i)-'0';
-			sumul[3] *= (int)right2.charAt(i)-'0';
+			sumul[2] += (int) right2.charAt(i) - '0';
+			sumul[3] *= (int) right2.charAt(i) - '0';
 		}
-		
+
 		for (int i = 0; i < sumul.length; i++) {
-			if(crong_Max < sumul[i]) {
+			if (crong_Max < sumul[i]) {
 				crong_Max = sumul[i];
 			}
 		}
-		
+
 		return crong_Max;
 	}
+
 /////////////////////////////////////////////////////////////////////////////////
 	// 8. 최종 pobi와 crong의 값 비교 return
-	public int finalGame(int pobi_Max,int crong_Max) {
-		if(pobi_Max > crong_Max) {
+	public int finalGame(int pobi_Max, int crong_Max) {
+		if (pobi_Max > crong_Max) {
 			return 1;
-		}else if(pobi_Max < crong_Max) {
+		} else if (pobi_Max < crong_Max) {
 			return 2;
-		}else if(pobi_Max == crong_Max) {
+		} else if (pobi_Max == crong_Max) {
 			return 0;
-		}else {
+		} else {
 			return -1;
 		}
 	}
